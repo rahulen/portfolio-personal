ab6b453bd9c053035f42e18039c564defbff2022
rahulen <94124587+rahulen@users.noreply.github.com>
2025-01-14 14:23:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore"; 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRL3zYqAKb20ACqOy10peP751doomFVso",
+  apiKey: "",
   authDomain: "protfoliorahul.firebaseapp.com",
-  projectId: "protfoliorahul",
-  storageBucket: "protfoliorahul.firebasestorage.app",
-  messagingSenderId: "202131921764",
-  appId: "1:202131921764:web:2a14cc58d9b2eba93b3871",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
